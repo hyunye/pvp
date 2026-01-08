c7cc92e6facc9d6ef6d4d1cdc444625f32f792a6
hyunye <a01021266135@gmail.com>
2026-01-08 22:06:04 +0900

    remove compiled js files

--- index.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const config = JSON.parse(fs.readFileSync('./server/config.json', 'utf-8'));
-config.debug = false;
-fs.writeFileSync('./server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-import('./server/index.js');
